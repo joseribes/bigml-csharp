eeb246791b868f51baca2af76091c6442b67cd8e
Jose Ribes <ribes@bigml.com>
2018-11-29 16:01:17 +0100

    Users tests

---
+++ Iris/DownloadDataset.cs
@@ -0,0 +1,41 @@
+﻿using BigML;
+using System; 
+using System.Threading.Tasks;
+
+namespace Iris
+{
+    class DownloadDataset
+    {
+        static void Main()
+        {
+            MainAsync().Wait();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Simple sample that runs through all steps to explicitly create a
+        /// local prediction from a csv file with the classic iris data.
+        /// </summary>
+        static async Task MainAsync()
+        {
+            // New BigML client with username and API key
+            var client = new Client("myuser", "8169dabca34b6ae5612a47b63dd97bead3bfeXXX");
+
+            // Get Dataset
+            string datasetId = "dataset/589ce5d7014404556a000308";
+            var dataset = await client.Get<DataSet>(datasetId);
+            // No push, so we need to busy wait for the dataset to be processed.
+            while ((dataset = await client.Get(dataset)).StatusMessage.NotSuccessOrFail())
+            {
+                await Task.Delay(10);
+            }
+            string fileName = "C:/Users/Jose/SourceCode/Repos/" + dataset.Id + ".csv";
+
+            // Download dataset
+            if (System.IO.File.Exists(fileName)) {
+                System.IO.File.Delete(fileName);
+            }
+            await client.DownloadDataset(datasetId, fileName);
+        }
+    }
+}
---
+++ Iris/ScriptList.cs
@@ -0,0 +1,42 @@
+﻿using BigML;
+using System; 
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    class ScriptList
+    {
+        static void Main()
+        {
+            MainAsync().Wait();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// List most recent scripts.
+        /// </summary>
+        static async Task MainAsync()
+        {
+            // New BigML client with username and API key
+            var client = new Client("jribes", "8169dabca34b6ae5612a47b63dd97bead3bfe8c4");
+
+            // Get Scripts
+            Ordered<Script.Filterable, Script.Orderable, Script> result
+                = (from s in client.ListScripts()
+                   orderby s.Created descending
+                   select s);
+
+            Listing<Script> scripts = await result; // await response
+            Execution.Arguments args = new Execution.Arguments();
+            args.Inputs = new List<dynamic[]>();
+            args.Inputs.Add(new dynamic[] {"dataset-id", "dataset/589ce5d7014404556a000308"});
+            foreach (Script scr in scripts)
+            {
+
+                Execution ex = await client.CreateExecution(scr, "C# test", args);
+                Console.WriteLine("Id: " + scr.Id + " - ResourceId: " + scr.Resource);
+            }
+        }
+    }
+}
